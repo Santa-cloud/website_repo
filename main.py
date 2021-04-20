8d44f5ca7eb5366d260e084ceff8178f0a3641be
Santa-py <72890462+Santa-py@users.noreply.github.com>
2021-04-20 21:00:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import requests
 from typing import Optional
 from pydantic import BaseModel
+import hashlib
 app = FastAPI()
 
 
@@ -38,9 +39,10 @@ def options_method():
 def post_method():
     return {"method": "POST"}
     
-@app.get("/auth?password={password}&password_hash={password_hash}", status_code)
-def auth(password, password_hash):
-    if (password == "haslo" and password_hash == "password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215"):
+@app.get("/auth")
+def auth(password: str, password_hash: str):
+    h_password = hashlib.sha512(password.encode())
+    if (h_password.hexdigset() == "013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215"):
         return {"auth": "ok", status_code=204}
     else:
         return {"auth": "wrong", status_code=401}
