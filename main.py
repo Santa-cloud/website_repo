6f66042f31541ce8a4dd73276f79a81ff2f170a3
Santa-py <72890462+Santa-py@users.noreply.github.com>
2021-04-20 22:07:50 +0200

    auth working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 import requests
 from typing import Optional
 from pydantic import BaseModel
@@ -40,9 +40,17 @@ def post_method():
     return {"method": "POST"}
     
 @app.get("/auth")
-def auth(password: str, password_hash: str):
-    h_password = hashlib.sha512(password.encode())
-    if (h_password.hexdigset() == "013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215"):
-        return {"auth": "ok", status_code=204}
+def auth(response: Response, password: Optional[str] = None, password_hash: Optional[str] = None):
+    if (password != None and password_hash != None):
+        e_password = password.encode()
+        h_password = hashlib.sha512(e_password)
+        
+        if (h_password.hexdigest() == "013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215"):
+            response.status_code = 204
+            return {"auth": "ok"}
+        else:
+            response.status_code = 401
+            return {"auth": "wrong"}           
     else:
-        return {"auth": "wrong", status_code=401}
+        response.status_code = 401
+        return {"auth": "wrong"}
