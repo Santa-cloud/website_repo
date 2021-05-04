dbe23c055b56a288938274b4082d612fffd78e9a
Santa-py <72890462+Santa-py@users.noreply.github.com>
2021-05-04 21:47:37 +0200

    L3ex2

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-from fastapi import FastAPI, Response, status, Request
+from fastapi import FastAPI, Response, status, Request, Depends, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 #from fastapi.staticfiles import StaticFiles
 from datetime import date, timedelta
 from typing import Optional
 from pydantic import BaseModel
 
 import hashlib
+import random
+import string
+import secrets
 
 app = FastAPI()
 app.id = 0
 app.secret_key = 'kashduhashdbahsdgahskdgasdgasdgsdgkjasfnuaevbczknckzygschkzsgckjhwz'
 app.cache = []
 app.access_tokens = []
-app.access_tokens.append(hashlib.sha256(f"4dm1n:NotSoSecurePa$${app.secret_key}".encode()).hexdigest())
+
+security = HTTPBasic()
+
 
 #app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
 
 
 @app.get("/")
@@ -102,17 +109,57 @@ def hello(request: Request):
     )
 
 
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, '4dm1n')
+    correct_password = secrets.compare_digest(credentials.password, 'NotSoSecurePa$$')
+    if not (correct_username and correct_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
+    return credentials.username, credentials.password
+
+
+def random_token():
+    token = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+    return hashlib.sha256(token.encode()).hexdigest()
+
+
+@app.post('/login_session', status_code=status.HTTP_201_CREATED, dependencies=[Depends(authenticate)])
+def login_session_view(response: Response):
+    session_token = random_token()
+    if len(app.session_token) == 0:
+        app.session_token.append(session_token)
+    else:
+        app.session_token[0] = session_token
+    response.set_cookie(key='session_token', value=session_token)
+    return {'message': 'You are logged'}
+
+
+@app.post('/login_token', status_code=status.HTTP_201_CREATED, dependencies=[Depends(authenticate)])
+def login_session_view():
+    json_token = random_token()
+    if len(app.json_token) == 0:
+        app.json_token.append(json_token)
+    else:
+        app.json_token[0] = json_token
+    return {'message': 'You are logged', "token": json_token}
+
+
+
+
+
+"""
 @app.post("/login_session")
 def login(response: Response, user: Optional[str] = None, password: Optional[str] = None):
     session_token = hashlib.sha256(f"{user}:{password}{app.secret_key}".encode()).hexdigest()
-    #app.access_tokens.append(session_token)
+    if app.access_tokens == 0:
+        app.access_tokens.append(session_token)
+
     response.set_cookie(key="session_token", value=session_token)
     if session_token in app.access_tokens:
         response.status_code = status.HTTP_201_CREATED
         return {"message": "Welcome"}
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return {"login": "unauthorized"}
+        return {"login": "not authorized"}
 
 
 @app.post("/login_token")
@@ -123,9 +170,9 @@ def login(response: Response, user: Optional[str] = None, password: Optional[str
         return {"token": session_token}
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return {"login": "unauthorized"}
+        return {"login": "not authorized"}
 
-
+"""
 
 '''
 @app.post("/login_session")
