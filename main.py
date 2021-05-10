4e022f7dc5012e2251a26c5899ab96ba4c42b4d2
Santa-py <72890462+Santa-py@users.noreply.github.com>
2021-05-11 00:30:21 +0200

    update l4 ex1

--- main.py
+++ main.py
@@ -27,21 +27,24 @@ async def products():
 
 @app.get("/categories")
 async def get_categories():
-    app.db_connection.row_factory = sqlite3.Row
-    data = app.db_connection.execute(
+    cursor = app.db_connection.cursor()
+    cursor.row_factory = sqlite3.Row
+    data = cursor.execute(
         "SELECT CategoryID, CategoryName FROM Categories ORDER BY CategoryID"
     ).fetchall()
+    return {
+        "categories":
+            [{"id": x['CategoryID'], "name": x["CategoryName"]} for x in data]
+    }
 
-    data["categories"] = [{"id": x['CategoryID'], "name": x["CategoryName"]} for x in data]
-    return data
-
-
+"""
 @app.get("/customers")
 async def get_categories():
     app.db_connection.row_factory = sqlite3.Row
     data = app.db_connection.execute(
         "SELECT CustomerID, CompanyName FROM Customers ORDER BY CustomerID"
     ).fetchall()
-
-    data["customers"] = [{"id": x['CustomerID'], "name": x["CustomerName"]} for x in data]
-    return data
+    customers = [{"id": x['CustomerID'], "name": x["CustomerName"]} for x in data]
+    #data["customers"] = customers
+    return customers
+"""
