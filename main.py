d94e6ff74adbb8ca4a781f5076172797306ceb87
Santa-py <72890462+Santa-py@users.noreply.github.com>
2021-05-11 02:06:51 +0200

    l4 ex3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 from fastapi import FastAPI, status, HTTPException
+from typing import Optional
 
 
 app = FastAPI()
@@ -65,3 +66,24 @@ async def get_product(product_id: int):
     if not product:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return product
+
+
+@app.get("/employees")
+async def get_employees(
+        limit: Optional[int] = -1,
+        offset: Optional[int] = 0,
+        order: Optional[str] = None
+):
+    if order is None:
+        order = "id"
+    elif order not in ["first_name", "last_name", "city"]:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    sqlite3.paramstyle = "named"
+    cursor = app.db_connection.cursor()
+    cursor.row_factory = sqlite3.Row
+    employees = cursor.execute(
+        "SELECT EmployeeID id, LastName last_name, FirstName first_name, City city "
+        f"FROM Employees ORDER BY {order} LIMIT :limit OFFSET :offset ",
+        {"limit": limit, "offset": offset}
+    ).fetchall()
+    return {"employees": employees}
